45680f16f43fd42a1fbaa3b0b5fd26dbd153dc2c
Trent Conley <trentconley@gmail.com>
2023-03-17 13:26:52 -0400

    added color changing as you load page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5296234daadb011"; }
+/******/ 		__webpack_require__.h = function() { return "1c55aad9b458c3ce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
